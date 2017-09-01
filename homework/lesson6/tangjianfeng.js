2e5f09e91dde22854c6a67eb2085bb25adc56c07
Tony <tangvwin@gmail.com>
2017-09-01 16:39:30 +0800

    fixed the bug of tangjianfeng.js

--- homework/lesson6/tangjianfeng.js
+++ homework/lesson6/tangjianfeng.js
@@ -1,11 +1,14 @@
 
+
+//同步方案
+
 //引入fs模块
 var fs = require('fs');
 
 //定义要查询的目标文件夹
 var dirPathString = '/Users/jianfengtang/words-from-the-heart';
 //定义读取要写入的文件的存放路径，名称和格式
-var outputPathString = './tang.txt';
+var outputPathString = './tangjianfeng.txt';
 
 //读取内容并赋值给写入文件
 var outPutFileContent = fs.readdirSync(dirPathString);
@@ -14,19 +17,19 @@ var outPutFileContent = fs.readdirSync(dirPathString);
 fs.writeFileSync(outputPathString, outPutFileContent);
 
 
-//异步方案
 
+//异步方案
 var fs =require('fs')
 var readPathString = '/Users/jianfengtang/words-from-the-heart';
-var writeFileString = './tang.txt';
-
+var writeFileString = './tangjianfeng1.txt';
 function callback(err, files) {
     if (err) {
          console.log('读取文件失败');
+         return;
     }
     if (files.length >0) {
         console.log(files);
-        fs.writeFile(writeFileString, fileContent, callback);
+        fs.writeFile(writeFileString, files, callbackwrite);
     } else {
         console.log('没有找到任何文件');
     }
@@ -39,8 +42,5 @@ function callback(err, files) {
     console.log('写文件成功');
   }
 }
-
-     
-
-
-fs.readdir(readPathString,callbackwrite );
+   
+fs.readdir(readPathString,callback );
