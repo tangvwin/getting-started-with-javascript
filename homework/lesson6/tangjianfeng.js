712d20e4b73864cfae7bd2c407eaa6216b0e77ff
Tony <tangvwin@gmail.com>
2017-09-01 10:09:08 +0800

    re fix

--- homework/lesson6/tangjianfeng.js
+++ homework/lesson6/tangjianfeng.js
@@ -1,9 +1,24 @@
+
+//引入fs模块
 var fs = require('fs');
 
-var dirPathString = '../../homework/lesson1';
+//定义要查询的目标文件夹
+var dirPathString = '/Users/jianfengtang/words-from-the-heart';
+//定义读取要写入的文件的存放路径，名称和格式
+var outputPathString = './tang.txt';
 
-var outputPathString = './write_lesson1_sync.txt';
-var files = fs.readdirSync('../../homework/lesson1')
+//读取内容并赋值给写入文件
+var outPutFileContent = fs.readdirSync(dirPathString);
+
+//调用fs的writeFileSync函数来写文件
+fs.writeFileSync(outputPathString, outPutFileContent);
+
+
+//异步方案
+
+var fs =require('fs')
+var readPathString = '/Users/jianfengtang/words-from-the-heart';
+var writeFileString = './tang.txt';
 
 function callback(err, files) {
     if (err) {
@@ -11,6 +26,7 @@ function callback(err, files) {
     }
     if (files.length >0) {
         console.log(files);
+        fs.writeFile(writeFileString, fileContent, callback);
     } else {
         console.log('没有找到任何文件');
     }
@@ -26,6 +42,5 @@ function callback(err, files) {
 
      
 
-console.log(files);
 
-fs.writeFileSync(outputPathString,files,callbackwrite() );
+fs.readdir(readPathString,callbackwrite );
